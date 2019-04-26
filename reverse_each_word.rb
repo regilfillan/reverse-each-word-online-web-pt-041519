b0a359219b1d0ec4b55da8ff2332e57c927f7c72
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-26 15:14:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,10 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
   
-  
-  
+def reverse_each_word(string)
+  string.split
+  array.each.collect do |word|
+    word.reverse
+  end
+  .join(" ")
+end
