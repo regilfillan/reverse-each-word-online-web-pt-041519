b125691d0a820e9e6ad0546756a4af4c14b3343b
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-26 15:08:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentance)
   original_array= sentance.split(", ")
   new_array= []
   original_array.each do |word|
-    puts "#{word.reverse}"
+    new_array << word.reverse
   end
   .join("' '")
 end
