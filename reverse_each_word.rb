e49d0def5d4590631b0c46e19dfa1d7d2fbcbbfa
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-26 15:11:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentance)
-  original_array= sentance.split
+def reverse_each_word(string)
+  original_array= string.split
   new_array= []
   original_array.each do |word|
     new_array << word.reverse
