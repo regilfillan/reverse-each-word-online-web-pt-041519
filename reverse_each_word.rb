a66e565ed3526349d61f30fc270865e441d4a9f4
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-26 14:53:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word("Hello there, and how are you?")
   array= "Hello there, and how are you?".split(", ")
   array.each do |word|
-    array << word.reverse
+    word.reverse
   end
   array.join("' '")
 end
